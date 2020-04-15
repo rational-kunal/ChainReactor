5581f5369a9365a3e8156f753fd823fe52406d50
kunal <kunalgk1999@gmail.com>
2020-04-15 23:18:15 +0530

    basic sphagetti code -- fastest prototype 3hr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Game from "./components/Game";
+
 function App() {
   return (
       <div className="App">
-
+        <Game gridSize={{ rowCount:6, columnCount:4 }} playerCount={2} />
       </div>
   );
 }
---
+++ src/core/ChainReactor.js
@@ -0,0 +1,165 @@
+class PlayerPoolController {
+    constructor(playerSize) {
+        this.playerSize = playerSize;
+        this.currentPlayer = 0;
+
+        this.wait = false;
+
+        this.defeatedPool = Array(this.playerSize).fill(false);
+        this.playerNRegion = Array(this.playerSize).fill(0);
+
+        this.moves = 0;
+    }
+
+    moveComplete() {
+        this.moves++;
+
+        this.currentPlayer++;
+        this.currentPlayer %= this.playerSize;
+        while (this.defeatedPool[this.currentPlayer]) {
+            this.currentPlayer++;
+            this.currentPlayer %= this.playerSize;
+        }
+    }
+
+    isComplete() {
+        let stillInGame = 0;
+        if ((this.moves > this.playerSize) && (!this.wait)) {
+            this.playerNRegion.forEach((n) => (n > 0) || stillInGame++);
+
+            return stillInGame === 1;
+        }
+
+        return false;
+    }
+}
+
+export default class ChainReactor {
+    grid = [];
+
+    constructor(gridSize, playerSize) {
+        this.gridSize = gridSize;
+
+        this.playerController = new PlayerPoolController(playerSize);
+
+        for (let row = 0; row < this.gridSize.row; row++) {
+            this.grid.push([]);
+            for (let col = 0; col < this.gridSize.column; col++) {
+                this.grid[row].push({
+                    value: 0,
+                    player: null
+                })
+            }
+        }
+    }
+
+    printGrid() {
+        for (let row = 0; row < this.gridSize.row; row++) {
+            console.log(this.grid[row]);
+        }
+    }
+
+    canAddAt(x, y) {
+        return ((this.grid[x][y].player == null) ||
+            (this.grid[x][y].player === this.playerController.currentPlayer));
+    }
+
+    canExplode(x, y) {
+        const value = this.grid[x][y].value;
+        if ((x === 0 && y === 0) ||
+            (x === 0 && y === this.gridSize.column - 1) ||
+            (x === this.gridSize.row - 1 && y === 0) ||
+            (x === this.gridSize.row - 1 && y === this.gridSize.column - 1)) {
+            return value === 1;
+        }
+
+        if ((x === 0) ||
+            (x === this.gridSize.row - 1) ||
+            (y === 0) ||
+            (y === this.gridSize.column - 1)) {
+            return value === 2;
+        }
+
+        return value === 3;
+    }
+
+    explosionIn(x, y) {
+        let explode_at = [{x: x - 1, y: y}, {x: x + 1, y: y}, {x: x, y: y - 1}, {x: x, y: y + 1}];
+        return explode_at.filter(({x, y}) => {
+            return (x >= 0) && (x < this.gridSize.row) && (y >= 0) && (y < this.gridSize.column)
+        })
+    }
+
+    reactionAt(x, y, fn) {
+        let reaction = {
+            change: {
+                value: 0,
+                player: null
+            },
+            explodeTo: []
+        };
+
+        if (this.canExplode(x, y)) {
+            reaction.explodeTo = this.explosionIn(x, y);
+            this.grid[x][y].value = 0;
+            this.playerController.playerNRegion[this.grid[x][y].player]--;
+            this.playerController.wait = true;
+            this.grid[x][y].player = null;
+        } else {
+            this.grid[x][y].value++;
+
+            this.playerController.playerNRegion[this.grid[x][y].player]--;
+            this.playerController.wait = false;
+            this.grid[x][y].player = this.playerController.currentPlayer;
+            reaction.change.player = this.playerController.currentPlayer;
+            this.playerController.playerNRegion[this.grid[x][y].player]++;
+        }
+        console.log(this.playerController.playerNRegion[this.playerController.currentPlayer]);
+
+        reaction.change.value = this.grid[x][y].value;
+
+        console.log(this.playerController.playerNRegion);
+
+        fn(reaction);
+    }
+
+    didMove(x, y) {
+        if (!this.canAddAt(x, y)) {
+            return null;
+        }
+
+        let addTo = [{x: x, y: y}];
+        let reactionChain = [];
+
+        while ((addTo.length !== 0) && (!this.playerController.isComplete())) {
+            let newAddTo = [];
+            let newChain = {
+                valueChangeAt: [],
+                explosions: []
+            };
+
+            addTo.forEach(({x, y}, index) => {
+                if (this.playerController.isComplete()) return;
+                this.reactionAt(x, y, ({change, explodeTo}) => {
+                    newChain.valueChangeAt.push({x: x, y: y, value: change.value, player: change.player});
+
+                    explodeTo.forEach((coordinate) => {
+                        newChain.explosions.push({
+                            ux: x, uy: y,
+                            vx: coordinate.x, vy: coordinate.y
+                        });
+
+                        newAddTo.push(coordinate);
+                    })
+                });
+            });
+
+            reactionChain.push(newChain);
+            addTo = newAddTo;
+        }
+
+        this.playerController.moveComplete();
+
+        return reactionChain;
+    }
+}
